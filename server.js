4f0324377f07f20206ee9fa27c641329ebc0b968
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-08 21:06:58 -0800

    folder structure

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "day_planner_db"
+});
+
+app.get("/", function(req, res) {
+   console.log("hello")
+  
+      res.end();
+    });
+
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
