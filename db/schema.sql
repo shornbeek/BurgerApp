09652a1b72274795f36220fa7844eb1d4a1197bf
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-13 22:17:30 -0800

    burger model

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- Drops the todolist if it exists currently --
+DROP DATABASE IF EXISTS db_burger;
+-- Creates the "todolist" database --
+CREATE DATABASE db_burger;
--- models/burgers.js
+++ models/burgers.js
@@ -1,11 +1,43 @@
 
 
- module.exports = function(sequelize, DataTypes) {
- var Burger = sequelize.define("Burger", {
-   title: {
-     burger_name: DataTypes.STRING,
-     devoured: false,
-   }
- });
- return Burger;
+//  module.exports = function(sequelize, DataTypes) {
+//  var Burger = sequelize.define("Burger", {
+//    title: {
+//      burger_name: DataTypes.STRING,
+//      devoured: false,
+//    }
+//  });
+//  return Burger;
+// };
+
+
+module.exports = function (sequelize, DataTypes) {
+    // Define the Burger Sequelize model
+    var Burger = sequelize.define("Burger",
+        {
+            // The name identifying the burger
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            // The availability boolean
+            devoured: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            }
+        }, {
+            classMethods: {
+                associate: function (models) {
+                    // Burger is associated with one customer
+                    Burger.belongsTo(models.Customer, {
+                        onDelete: "CASCADE",
+                        foreignKey: {
+                            allowNull: true
+                        }
+                    });
+                }
+            }
+        });
+
+    return Burger;
 };
