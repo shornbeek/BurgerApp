28497e9380ba3b44380bda95fd8fda528a6cbd88
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-14 22:17:01 -0800

    deploy test

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgerController.js");
 
-app.use('/',routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 
