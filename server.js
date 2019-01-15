b90395d8d8bab19d4d4638ddc51cc82789efb4d7
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-14 21:45:42 -0800

    styled need some sleep

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ let routes = require('./controllers/burgercontroller.js');
 app.use('/', routes);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
+// db.sequelize.sync({ force: true }).then(function() {
+  db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
