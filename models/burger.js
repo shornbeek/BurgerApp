2f63a013cf8cf41d565f01e393639ccc566f87a8
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-14 19:31:39 -0800

    fixed modal worked on views

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
-  var burgers = sequelize.define("burgers", {
-    burger_name: DataTypes.STRING,
-    devoured: DataTypes.BOOLEAN
-  });
-  return burgers;
-};
+// module.exports = function(sequelize, DataTypes) {
+//   var burgers = sequelize.define("burgers", {
+//     burger_name: DataTypes.STRING,
+//     devoured: DataTypes.BOOLEAN
+//   });
+//   return burgers;
+// };
 
+module.exports = function(sequelize, DataTypes) {
+  
+  let Burger = sequelize.define("burger", {
+    
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN
+    }
+    
+  });
+  return Burger;
+};
